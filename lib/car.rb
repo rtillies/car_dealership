b2016c843df3391c41864ddf4b16dc6e8699864f
Richard Tillies <rtillies@gmail.com>
2021-12-08 09:55:24 -0500

    test: Car has a model

--- lib/car.rb
+++ lib/car.rb
@@ -1,8 +1,9 @@
 class Car
-  attr_reader :make
-  
+  attr_reader :make, :model
+
   def initialize(make_model, payment, length)
     string = make_model.split(" ")
     @make = string[0]
+    @model = string[1]
   end
 end
