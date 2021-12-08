8273b1c248baa50a7c05ec0363b1aad4ea0ac734
Richard Tillies <rtillies@gmail.com>
2021-12-08 09:56:01 -0500

    test: Car has a monthly payment

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,10 @@
 class Car
-  attr_reader :make, :model
+  attr_reader :make, :model, :monthly_payment
 
   def initialize(make_model, payment, length)
     string = make_model.split(" ")
     @make = string[0]
     @model = string[1]
+    @monthly_payment = payment
   end
 end
