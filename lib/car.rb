87aae895b27676120b5f46a2ab9bfde2f0e4dcde
Richard Tillies <rtillies@gmail.com>
2021-12-08 09:56:57 -0500

    test: Car has a loan length

--- lib/car.rb
+++ lib/car.rb
@@ -1,10 +1,14 @@
 class Car
-  attr_reader :make, :model, :monthly_payment
+  attr_reader :make,
+              :model,
+              :monthly_payment,
+              :loan_length
 
   def initialize(make_model, payment, length)
     string = make_model.split(" ")
     @make = string[0]
     @model = string[1]
     @monthly_payment = payment
+    @loan_length = length
   end
 end
